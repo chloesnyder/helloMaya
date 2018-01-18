61398655fa199acae038fa216b7a73ec2a7bea0c
Chloe Snyder <csny@seas.upenn.edu>
2018-01-18 14:44:07 -0500

    Add project files.

---
+++ helloMaya/helloMayaCmd.cpp
@@ -0,0 +1,44 @@
+//
+// Copyright (C) 
+// 
+// File: helloMayaCmd.cpp
+//
+// MEL Command: helloMaya
+//
+// Author: Maya Plug-in Wizard 2.0
+//
+
+// Includes everything needed to register a simple MEL command with Maya.
+// 
+#include <maya/MSimple.h>
+
+// Use helper macro to register a command with Maya.  It creates and
+// registers a command that does not support undo or redo.  The 
+// created class derives off of MPxCommand.
+//
+DeclareSimpleCommand( helloMaya, "", "2015");
+
+MStatus helloMaya::doIt( const MArgList& args )
+//
+//	Description:
+//		implements the MEL helloMaya command.
+//
+//	Arguments:
+//		args - the argument list that was passes to the command from MEL
+//
+//	Return Value:
+//		MS::kSuccess - command succeeded
+//		MS::kFailure - command failed (returning this value will cause the 
+//                     MEL script that is being run to terminate unless the
+//                     error is caught using a "catch" statement.
+//
+{
+	MStatus stat = MS::kSuccess;
+
+	// Since this class is derived off of MPxCommand, you can use the 
+	// inherited methods to return values and set error messages
+	//
+	setResult( "helloMaya command executed!\n" );
+
+	return stat;
+}
