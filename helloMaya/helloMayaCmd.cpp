c063ee4bf34a6f7f82892b78f1f016b167fc6077
Chloe Snyder <csny@seas.upenn.edu>
2018-01-18 15:27:32 -0500

    progress

--- helloMaya/helloMayaCmd.cpp
+++ helloMaya/helloMayaCmd.cpp
@@ -12,12 +12,26 @@
 // 
 #include <maya/MSimple.h>
 #include <maya/MGlobal.h>
+#include <maya/MSyntax.h>
+#define NFLAG "-name"
+#define NFLAGLONG "-name"
+#define IDFLAG "-id"
+#define IDFLAGLONG "-id"
+
 
 // Use helper macro to register a command with Maya.  It creates and
 // registers a command that does not support undo or redo.  The 
 // created class derives off of MPxCommand.
 //
 DeclareSimpleCommand( helloMaya, "", "2015");
+//http://download.autodesk.com/global/docs/mayasdk2012/en_us/index.html?url=files/Command_plugins_Flags.htm,topicNumber=d28e2752
+MSyntax helloMaya::newSyntax() {
+	MSyntax syntax;
+	syntax.addFlag(NFLAG, NFLAGLONG, MSyntax::kString);
+	syntax.addFlag(IDFLAG, IDFLAGLONG, MSyntax::kString);
+	syntax.makeFlagMultiUse(NFLAG);
+	return syntax;
+}
 
 MStatus helloMaya::doIt( const MArgList& args )
 //
@@ -34,6 +48,9 @@ MStatus helloMaya::doIt( const MArgList& args )
 //                     error is caught using a "catch" statement.
 //
 {
+
+	MSyntax syntax = newSyntax();
+
 
 	MString dialogBox = "confirmDialog -message \"name:  id:\" -button \"OK\" -title \"Hello World\";";	//"confirmDialog -title \"Hello World!\" -message \"name: +";
 
